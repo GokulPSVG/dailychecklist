cc9c09835fcca7dbf6277582b88868cb4a0bd27e
GokulPSVG <creatcoc0001@gmail.com>
2023-10-01 13:38:27 +0530

    fourteenth commit

--- dailychecklist/src/Footer.js
+++ dailychecklist/src/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-        <p>CopyRight&copy;Gokul Pavunraj</p>
+        <p>CopyRight&copy; Gokul Pavunraj</p>
     </footer>
   )
 }
