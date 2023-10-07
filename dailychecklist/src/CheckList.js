f552aeaee27fa93c8c012d21c07b67e3abf2b6f1
GokulPSVG <creatcoc0001@gmail.com>
2023-10-07 23:39:01 +0530

    eighteenth commit

--- dailychecklist/src/CheckList.js
+++ dailychecklist/src/CheckList.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ListItem from './ListItem'
 
 
 const CheckList = ({listContent,handelCheck,handelDelete}) => {
-    const randomNumber=Math.round(Math.random()*3)
-    const quote1="YOU ARE GREAT AND YOU CAN ACHIEVE ANYTHING"
-    const quote2='THE FUTURE DEPENDS ON WHAT YOU DO TODAY'
-    const quote3="TODAY IS ANOTHER CHANCE TO GET BETTER"
-    const quote4="IT REMAINS IMPOSSIBLE UNTIL IT'S DONE"
-    const line = randomNumber=== 1 ? quote1 : randomNumber === 2 ? quote2 : randomNumber === 3 ? quote3 : quote4 ;
+    const [line,setline]=useState("IT REMAINS IMPOSSIBLE UNTIL IT'S DONE")
+    useEffect(()=>{
+        const randomNumber=Math.round(Math.random()*3)
+        const quote1="YOU ARE GREAT AND YOU CAN ACHIEVE ANYTHING"
+        const quote2='THE FUTURE DEPENDS ON WHAT YOU DO TODAY'
+        const quote3="TODAY IS ANOTHER CHANCE TO GET BETTER"
+        const quote4="IT REMAINS IMPOSSIBLE UNTIL IT'S DONE"
+        const line = randomNumber=== 1 ? quote1 : randomNumber === 2 ? quote2 : randomNumber === 3 ? quote3 : quote4 ;
+        setline(line)
+    },[])
   return (
     <main>
         <div className='quote'>
